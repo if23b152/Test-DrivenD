336c20e90411fabcae7392a87ce73564cb41c3dd
if23b152 <161253553+if23b152@users.noreply.github.com>
2024-05-12 15:12:24 +0200

    Update spaceship_finder_spec.js
    hatte den spaceship auf [2, 2] und damit die ganze zeit gerechnet. musste dann auf [7, 2] umändern

--- Help_ALF/spec/spaceship_finder_spec.js
+++ Help_ALF/spec/spaceship_finder_spec.js
@@ -1,11 +1,24 @@
 describe("findSpaceship", function() {
     it("should find the spaceship at the correct coordinates", function() {
-        var map = "....\n....\n..X.\n....";
-        expect(findSpaceship(map)).toEqual([7, 2]);
+        var map = ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  "..X.....\n";
+        expect(findSpaceship(map)).toEqual([7, 2]); 
     });
-
     it("should return 'Spaceship lost forever.' if no spaceship is found", function() {
-        var map = "....\n....\n....\n....";
+        var map = ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n" +
+                  ".......\n";
         expect(findSpaceship(map)).toBe("Spaceship lost forever.");
     });
 });
