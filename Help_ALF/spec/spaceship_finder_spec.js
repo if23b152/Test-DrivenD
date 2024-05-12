18a6fccaad5309c4e51a534e5ca1e0b6dd43e2a7
if23b152 <161253553+if23b152@users.noreply.github.com>
2024-05-12 22:09:26 +0200

    Update spaceship_finder_spec.js
    sucht ob es nach unten gerollt ist

--- Help_ALF/spec/spaceship_finder_spec.js
+++ Help_ALF/spec/spaceship_finder_spec.js
@@ -10,6 +10,18 @@ describe("findSpaceship", function() {
                   "..X.....\n";
         expect(findSpaceship(map)).toEqual([7, 2]); 
     });
+        it("should find the spaceship at the bottom coordinates", function() {
+        var map = ".......\n" +
+                    ".......\n" +
+                    ".......\n" +
+                    ".......\n" +
+                    ".......\n" +
+                    ".......\n" +
+                    "........\n"+
+                    ".......\n"+
+                    "..X....\n";
+        expect(findSpaceship(map)).toEqual([8, 2]);
+    })
     it("should return 'Spaceship lost forever.' if no spaceship is found", function() {
         var map = ".......\n" +
                   ".......\n" +
