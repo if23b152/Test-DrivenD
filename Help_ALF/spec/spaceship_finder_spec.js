2aa3dff1d2d98713fab0c08f9c1fff2c72c99f78
if23b152 <161253553+if23b152@users.noreply.github.com>
2024-05-12 14:38:53 +0200

    Update spaceship_finder_spec.js
    ersten testfall

---
+++ Help_ALF/spec/spaceship_finder_spec.js
@@ -0,0 +1,11 @@
+describe("findSpaceship", function() {
+    it("should find the spaceship at the correct coordinates", function() {
+        var map = "....\n....\n..X.\n....";
+        expect(findSpaceship(map)).toEqual([2, 2]);
+    });
+
+    it("should return 'Spaceship lost forever.' if no spaceship is found", function() {
+        var map = "....\n....\n....\n....";
+        expect(findSpaceship(map)).toBe("Spaceship lost forever.");
+    });
+});
