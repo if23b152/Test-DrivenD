a3e540aff37ff49a954a215acc80e0f373d9d446
if23b152 <161253553+if23b152@users.noreply.github.com>
2024-05-12 22:13:55 +0200

    Update spaceship_finder_spec.js
    raumschiff sollte eins drüber sein

--- Help_ALF/spec/spaceship_finder_spec.js
+++ Help_ALF/spec/spaceship_finder_spec.js
@@ -22,6 +22,18 @@ describe("findSpaceship", function() {
                     "..X....\n";
         expect(findSpaceship(map)).toEqual([8, 2]);
     })
+    it("should find the spaceship one upper the true coordinates", function() {
+    var map = ".......\n" +
+                ".......\n" +
+                ".......\n" +
+                ".......\n" +
+                ".......\n" +
+                ".......\n" +
+                "..X.....\n"+
+                ".......\n"+
+                ".......\n";
+    expect(findSpaceship(map)).toEqual([6, 2]);
+})
     it("should return 'Spaceship lost forever.' if no spaceship is found", function() {
         var map = ".......\n" +
                   ".......\n" +
